4a11501a5990cbafa7df165da7e197fa97cac0a5
Erick <erick@farwell-consultants.com>
2025-10-28 11:40:18 +0300

    added-api-endpoints

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import {
   getChatLogs,
   getChatLogByPlatform,
@@ -6,10 +11,37 @@ import {
 } from '../controllers/chatLogController.js';
 
 const router = express.Router();
+// console.log(process.env.APP_HMAC_KEY);
+export const verifyRequest = (req, res, next) => {
+  const internalKey = req.headers["x-internal-key"];
+  const authHeader = req.headers.authorization;
 
+//   console.log(internalKey, process.env.INTERNAL_API_KEY);
+  //  Internal requests
+  if (internalKey && internalKey === process.env.INTERNAL_API_KEY) {
+    req.authSource = "internal";
+    return next();
+  }
+
+  // JWT external requests
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+    try {
+      const decoded = jwt.verify(token, process.env.APP_HMAC_KEY);
+      req.authSource = decoded.app || "external";
+      return next();
+    } catch (err) {
+      return res.status(401).json({ message: "Invalid or expired token" });
+    }
+  }
+
+  //  Unauthorized
+  return res.status(401).json({ message: "Unauthorized" });
+};
 //Routes to perform chatlogs crud operations
-router.get('/chatLogs', getChatLogs);
-router.get('/chatLogs/:id', getChatLogByPlatform);
+router.get('/chatLogs', verifyRequest, getChatLogs);
+router.get('/chatLogs/:id', verifyRequest, getChatLogByPlatform);
 router.post('/chatLogs', createChatLog);
 
 export default router;
+
